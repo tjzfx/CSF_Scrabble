c05b55f4757180ca2890406ffcb85214705d5d2b
Thomas Zoltner <thomas.zoltner@warbyparker.com>
2014-02-24 21:29:20 -0500

    Need to build string I/O

--- src/com/example/scrabble/MyActivity.java
+++ src/com/example/scrabble/MyActivity.java
@@ -2,14 +2,24 @@ package com.example.scrabble;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+EditText edit;
+TextView text;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    edit = (EditText)findViewById(R.id.);
+
 }
