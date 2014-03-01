8f5e88296db836cf560bb03b1bcad8b5b833fd5a
Thomas Zoltner <thomas.zoltner@warbyparker.com>
2014-03-01 13:19:17 -0500

    Add untracked files

--- src/com/example/scrabble/MyActivity.java
+++ src/com/example/scrabble/MyActivity.java
@@ -2,8 +2,10 @@ package com.example.scrabble;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
@@ -11,13 +13,23 @@ public class MyActivity extends Activity {
      * Called when the activity is first created.
      */
 
-EditText edit;
-TextView text;
+    Button mButton;
+    EditText mEdit;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
 
-}
+    mButton = (Button)findViewById(R.id.button);
+    mEdit   = (EditText)findViewById(R.id.textView10);
+
+    mButton.setOnClickListener(
+            new View.OnClickListener()
+    {
+        public void onClick(View view)
+        {
+            Log.v("EditText", mEdit.getText().toString());
+        }
+    });
+}}
